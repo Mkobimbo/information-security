4e5a5abc10ec86cc94d8fe6bc4aaa8962b96a27f
Mkobimbo <122867985+Mkobimbo@users.noreply.github.com>
2023-11-20 18:42:25 +0300

    Update myApp.js

--- boilerplate-infosec/myApp.js
+++ boilerplate-infosec/myApp.js
@@ -19,7 +19,9 @@ app.use(helmet.noSniff())
 //Prevent IE from Opening Untrusted HTML with helmet.ieNoOpen()
 app.use(helmet.ieNoOpen());
 
-
+//Ask Browsers to Access Your Site via HTTPS Only with helmet.hsts()
+var ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true}))
 
 
 
