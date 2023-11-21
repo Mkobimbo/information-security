6b436e705fa8c1920b927d75b5bda10a77964e2e
Mkobimbo <122867985+Mkobimbo@users.noreply.github.com>
2023-11-21 17:22:16 +0300

    Update myApp.js

--- boilerplate-infosec/myApp.js
+++ boilerplate-infosec/myApp.js
@@ -36,6 +36,20 @@ app.use(helmet.csp({ directives: {
   
 }}));
 
+//app.use(helmet()) will automatically include all the middleware introduced above, except noCache(), and contentSecurityPolicy(), but these can be enabled if necessary. You can also disable or configure any other middleware individually, using a configuration object.
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}))
+
 
 
 
